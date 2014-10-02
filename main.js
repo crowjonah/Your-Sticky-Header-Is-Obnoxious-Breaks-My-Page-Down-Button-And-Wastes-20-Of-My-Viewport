5a8bbc402ef6d349c02ed6ebdf8ef3041664fe33
crowjonah <crowjonah@gmail.com>
2014-10-02 15:07:48 -0400

    remove jQuery dependency, throttle scroll events to prevent jank

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-$(document).on("scroll", function() {
-    $('div, header, nav').filter(function() {
-        if ($(this).css("position") === 'fixed') {
-            if (parseInt($(this).offset().top, 10) == parseInt($(document).scrollTop(), 10)) {
-                $(this).remove();
-            }
-        }
-    });
+var fixedFools = [];
+
+window.onscroll = Cowboy.throttle(250, function() {
+	var possibleOffenders = document.querySelectorAll('div:not([data-fix-watching="true"]), header:not([data-fix-watching="true"]), nav:not([data-fix-watching="true"])'),
+		newFools = Array.prototype.filter.call(possibleOffenders, function (node) {
+			var isFixed = window.getComputedStyle(node).getPropertyValue('position') == 'fixed',
+				isTopHat = node.getBoundingClientRect().top + window.pageYOffset == document.querySelector('body').scrollTop;
+
+			if (isFixed && isTopHat)
+			{
+				node.setAttribute('data-old-display', node.style.display);
+				node.setAttribute('data-fix-watching', 'true');
+				node.style.display = 'none';
+			}
+
+			return isFixed && isTopHat;
+		});
+
+	fixedFools = fixedFools.concat(newFools);
+
+	if (window.pageYOffset <= 0)
+	{
+		for (var i = 0; i < fixedFools.length; i++)
+		{
+			fixedFools[i].style.display = fixedFools[i].getAttribute('data-old-display');
+			fixedFools[i].setAttribute('data-fix-watching', 'false');
+		}
+	}
 });
